54f218ffe51006d371197ad176909fa49b27919d
p0937507934 <p0937507934@gmail.com>
2019-05-31 00:22:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith(".hello"):
-        msg="Hello (0.author.mention) How are you today".format(message)
+        msg="Hello {0.author.mention} How are you today".format(message)
         await client.send_message(message.channel,msg)
     if message.content.startswith(".bye"):
-        msg="Goodbye  (0.author.mention) Hope To See You Again Soon :wave:".format(message)
+        msg="Goodbye  {0.author.mention} Hope To See You Again Soon :wave:".format(message)
         await client.send_message(message.channel, msg)
 
 client.run(os.getenv('TOKEN'))
