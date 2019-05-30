802762688ed7fac0f2487d39a9637f33b647cd1e
p0937507934 <p0937507934@gmail.com>
2019-05-31 00:11:55 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import os
+
+Client=discord.Client()
+client =commands.Bot(command_prefix=".")
+@client.event
+async def on_ready():
+    print("Thankyou for using video bot")
+    await client.change_presence(game=discord.Game(name="videos"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(".hello"):
+        msg="Hello (0.author.mention) How are you today".format(message)
+        await client.send_message(message.channel,msg)
+    if message.content.startswith(".bye"):
+        msg="Goodbye  (0.author.mention) Hope To See You Again Soon :wave:".format(message)
+        await client.send_message(message.channel, msg)
+
+client.run(os.getenv('TOKEN'))
